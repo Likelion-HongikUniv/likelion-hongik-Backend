424b4dd9a92ec828f7c55a10130994afdcb23b80
jetty <dlwnsdud980727@gmail.com>
2023-01-07 23:36:23 +0900

    Project setting

---
+++ src/main/java/Likelion/Recruiting/RecruitingApplication.java
@@ -0,0 +1,13 @@
+package Likelion.Recruiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Likelion/Recruiting/RecruitingApplicationTests.java
@@ -0,0 +1,13 @@
+package Likelion.Recruiting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
