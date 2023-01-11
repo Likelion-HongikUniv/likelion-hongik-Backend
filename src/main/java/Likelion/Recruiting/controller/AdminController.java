117fa08ae6953bde0c81909483b539363c532928
jetty <dlwnsdud980727@gmail.com>
2023-01-11 14:39:44 +0900

    First commit

---
+++ src/main/java/Likelion/Recruiting/controller/AdminController.java
@@ -0,0 +1,22 @@
+package Likelion.Recruiting.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class AdminController {
+
+    @RequestMapping("/admin")
+    private String main() {
+        return "admin/main";
+    }
+
+    @GetMapping("/admin/login")
+    public String login() {
+        return "admin/login";
+    }
+}
+
