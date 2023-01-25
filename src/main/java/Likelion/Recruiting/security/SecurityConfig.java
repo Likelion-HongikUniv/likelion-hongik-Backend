5eb4847a118655587e286a435144fb9da13f457a
jetty <dlwnsdud980727@gmail.com>
2023-01-25 16:12:55 +0900

    [Feat] : 관리자 페이지 접근제어기능 추가

--- src/main/java/Likelion/Recruiting/security/SecurityConfig.java
+++ src/main/java/Likelion/Recruiting/security/SecurityConfig.java
@@ -1,24 +1,44 @@
 package Likelion.Recruiting.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("1").password(encoder.encode("1")).roles("ADMIN").build());
+
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Order(1)
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
-                .antMatcher("/admin/") // 해당 경로는 접근 막겠다. 이 코드 없으면 전부 다막힘
                 .authorizeRequests()
-                .anyRequest().authenticated()
-                .and().formLogin();
+                .anyRequest().permitAll()
+                .and()
+                .formLogin();
 
         return http.build();
-
     }
+
 }
